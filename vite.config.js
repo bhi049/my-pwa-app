16c2e8a3e2f170724f6618569366d2a509d7da07
bhi049 <bhi049@myy.haaga-helia.fi>
2025-06-30 15:46:47 +0300

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,34 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/my-pwa-app/",
+  base: '/my-pwa-app/',
   plugins: [
     react(),
     VitePWA({
-      registertype: "autoUpdate",
+      registerType: 'autoUpdate',
       manifest: {
-        name: "my Test PWA",
-        short_name: "TestPWA",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#000000",
+        name: 'My PWA Test App',
+        short_name: 'MyPWA',
+        start_url: '/my-pwa-app/',
+        scope: '/my-pwa-app/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
         icons: [
           {
-            src: "icon-192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            src: "icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 });
