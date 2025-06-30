b415e2e8f32cdbd5ab6051e4ce770ffa311e9a3e
bhi049 <bhi049@myy.haaga-helia.fi>
2025-06-30 15:50:23 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,20 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/my-pwa-app/',
+  base: '/my-pwa-app/',        // tämä on tärkeä GitHub Pagesille
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
-        name: 'My PWA Test App',
-        short_name: 'MyPWA',
+        name: 'my Test PWA',
+        short_name: 'TestPWA',
         start_url: '/my-pwa-app/',
         scope: '/my-pwa-app/',
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#000000',
+        lang: 'en',
         icons: [
           {
             src: 'icon-192.png',
