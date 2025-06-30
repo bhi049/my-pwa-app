bbdb602a875de25226c68fc375a40c50fafb5488
bhi049 <bhi049@myy.haaga-helia.fi>
2025-06-30 15:34:52 +0300

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registertype: "autoUpdate",
+      manifest: {
+        name: "my Test PWA",
+        short_name: "TestPWA",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
